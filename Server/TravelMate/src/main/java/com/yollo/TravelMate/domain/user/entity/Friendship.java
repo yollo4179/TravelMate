6a20bf99068db004dcb1c2abb71c8029f99c9417
yollo4179 <166979316+yollo4179@users.noreply.github.com>
2026-05-31 22:50:47 +0900

    Refactor :[친구 테이블 JPA 스타일로 수정]

--- Server/TravelMate/src/main/java/com/yollo/TravelMate/domain/user/entity/Friendship.java
+++ Server/TravelMate/src/main/java/com/yollo/TravelMate/domain/user/entity/Friendship.java
@@ -1,21 +1,33 @@
 package com.yollo.TravelMate.domain.user.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "friendships")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Friendship {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "friendship_id")
 	private Long friendshipId; //pk
-	private String userId;	//fk
-	private String friendId;	//fk
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", referencedColumnName = "uid", nullable = false)
+	private User user;	//fk
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "friend_id", referencedColumnName = "uid", nullable = false)
+	private User friend;	//fk
+
+	@Column(length = 20)
 	private String status; // PENDING, ACCEPTED, REJECTED
-	
-	
 }
