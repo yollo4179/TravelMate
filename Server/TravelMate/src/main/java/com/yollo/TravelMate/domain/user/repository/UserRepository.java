9319a025bd3baffca64a8b7ee2737e240129e703
yollo4179 <166979316+yollo4179@users.noreply.github.com>
2026-05-31 22:49:23 +0900

    REFACTOR : 친구 테이블 JPA 스타일로 변경

--- Server/TravelMate/src/main/java/com/yollo/TravelMate/domain/user/repository/UserRepository.java
+++ Server/TravelMate/src/main/java/com/yollo/TravelMate/domain/user/repository/UserRepository.java
@@ -7,11 +7,10 @@ import com.yollo.TravelMate.domain.user.entity.User;
 
 //JPA라는 비서에게 우리 DB 관리를 맡기는 계약서"
 public interface UserRepository extends JpaRepository<User, String>{
-	//User 엔티티와 연결된 users 테이블을 관리
-	//String: "User 엔티티에서 **@Id(PK)**로 지정한 필드의 타입이 **string**이야."라고 알려
+	
 	Optional<User> findByEmail(String email);
 	Optional<User> findByUserId(String userId);
-	// 사용자 아이디(UID)로 사용자 찾기 (JWT 토큰에서 추출한 정보로 사용자 조회 시 사용)
+	
     Optional<User> findByUid(String uid);
     
     boolean existsByUserId(String userId);
