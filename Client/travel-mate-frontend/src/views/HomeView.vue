8a0b941513100afcd49316f267e1632de06b55e6
yollo4179 <166979316+yollo4179@users.noreply.github.com>
2026-05-31 22:43:32 +0900

    ETC: 설정들 추가

--- Client/travel-mate-frontend/src/views/HomeView.vue
+++ Client/travel-mate-frontend/src/views/HomeView.vue
@@ -1,7 +1,27 @@
 <template>
-  <div></div>
+  <h1>홈 화면</h1>
+  <p>여기는 홈 화면입니다. 로그인 여부에 따라 다른 콘텐츠를 보여줄 수 있습니다.</p>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+/* Scrollbar configurations for clean layout */
+.scrollbar-none::-webkit-scrollbar {
+  display: none;
+}
+.scrollbar-none {
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+.scrollbar-thin::-webkit-scrollbar {
+  width: 4px;
+}
+.scrollbar-thin::-webkit-scrollbar-track {
+  background: transparent;
+}
+.scrollbar-thin::-webkit-scrollbar-thumb {
+  background: #334155;
+  border-radius: 9999px;
+}
+</style>
